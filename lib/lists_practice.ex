5d59a11a7673c4e437bf82b0fd27979d6993a025
Your Name <you@example.com>
2023-04-10 10:39:11 -0700

    Complete and working len() and sum() list functions

--- lib/lists_practice.ex
+++ lib/lists_practice.ex
@@ -12,7 +12,11 @@ defmodule ListsPractice do
       :world
 
   """
-  def hello do
-    :world
-  end
+  def len([]), do: 0
+  def len([_head | tail]), do: 1 + len(tail)
+  ##Pattern matching for nonempty list: splits remaining list into a head and a tail.
+
+  def sum([]), do: 0
+  def sum([head | tail]), do: head + sum(tail)
+  #Function to sum elements of list.  
 end
